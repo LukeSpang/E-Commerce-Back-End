782a27b72b60555ec6edf8040f3edfe4c56829d3
Luke Spang <lspang20@gmail.com>
2021-06-27 11:44:50 -0400

    model connections

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreginKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreginKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreginKey: 'product_id',
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreginKey: 'tag_id',
+});
 
 module.exports = {
   Product,
